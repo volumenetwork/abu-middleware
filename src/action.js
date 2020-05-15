6d1ef9395b32723c4d1e297a3c55ce850cb000a5
dpascu <dany3pp@gmail.com>
2020-05-15 10:53:01 +0200

    fixed upercase letter in key

--- src/action.js
+++ src/action.js
@@ -2,16 +2,16 @@ export const Type = '@@AZURE_BLOCK_UPLOAD';
 
 /**
  * Redux action to upload a file to Azure Blob Storage by using a sas url
- * @param {String} sasUrl
+ * @param {String} sasurl
  * @param {File} file
  * @param {Object} callbacks
  * @param {Function} callbacks.onError
  * @param {Function} callbacks.onProgress
  * @param {Function} callbacks.onSuccess
  */
-const azureBlockUploadAction = (sasUrl, file, callbacks = {}) => ({
+const azureBlockUploadAction = (sasurl, file, callbacks = {}) => ({
   type: Type,
-  payload: { endpoint: sasUrl, file, callbacks },
+  payload: { endpoint: sasurl, file, callbacks },
 });
 
 export default azureBlockUploadAction;
